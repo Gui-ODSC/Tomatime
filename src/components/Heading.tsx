da908420037def0e4faed3910d84326e075a24d3
Guilherme Oliveira <gui.dev.oliveira@gmail.com>
2025-04-28 22:00:22 -0300

    chore: Adicionado o componente de heading

---
+++ src/components/Heading.tsx
@@ -0,0 +1,11 @@
+import styles from './Heading.module.css'
+
+export default function Heading(props) {
+    return (
+        <>
+            <h1 className={styles.heading}>
+                {props.children}
+            </h1>
+        </>
+    )
+}
