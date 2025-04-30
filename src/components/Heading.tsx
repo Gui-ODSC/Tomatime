c95368659a8e2cc8653b03f49000d8d3620e41ff
Guilherme Oliveira <gui.dev.oliveira@gmail.com>
2025-04-29 23:04:33 -0300

    chore: Adicionada a tipagem para o children que será recebido do App

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import styles from './Heading.module.css'
 
-export default function Heading(props) {
-    return (
-        <>
-            <h1 className={styles.heading}>
-                {props.children}
-            </h1>
-        </>
-    )
+type HeadingProps = {
+	children: React.ReactNode;
+}
+
+export default function Heading({ children }: HeadingProps) {
+	return (
+		<>
+			<h1 className={styles.heading}>
+				{children}
+			</h1>
+		</>
+	)
 }
