c1f5a92b33d76417524d3d10081ecfdf7f7df587
Guilherme Oliveira <gui.dev.oliveira@gmail.com>
2025-04-29 23:03:30 -0300

    chore: Adicionado (button e icon) como children no componente de Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles/theme.css'
 import './styles/global.css'
 import Heading from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export default function App() {
 	return (
 		<>
-			<Heading>Heading</Heading>
+			<Heading>
+				Heading
+				<button>
+					<TimerIcon />
+				</button>
+			</Heading>
 			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam optio nemo ratione quibusdam asperiores alias est nulla qui nobis non, aspernatur laborum. Excepturi molestiae voluptates dolorum aspernatur! Magni, ipsa nihil!</p>
 		</>
 	)
