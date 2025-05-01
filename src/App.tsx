a676a4cee489dbf344096f26023f7a6847596163
Guilherme Oliveira <gui.dev.oliveira@gmail.com>
2025-04-30 22:11:35 -0300

    chore: Adicionadas as seções para a tela home

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import './styles/theme.css'
 import './styles/global.css'
-import Heading from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export default function App() {
 	return (
 		<>
-			<Heading>
-				Heading
-				<button>
-					<TimerIcon />
-				</button>
-			</Heading>
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam optio nemo ratione quibusdam asperiores alias est nulla qui nobis non, aspernatur laborum. Excepturi molestiae voluptates dolorum aspernatur! Magni, ipsa nihil!</p>
+			<div className='container'>
+				<div className='content'>
+					<section>
+						logo
+					</section>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='content'>
+					<section>
+						Menu
+					</section>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='content'>
+					<section>
+						Form
+					</section>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='content'>
+					<section>
+						Footer
+					</section>
+				</div>
+			</div>
 		</>
 	)
 }
