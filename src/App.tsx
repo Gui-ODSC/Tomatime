6276832567b7684346bc5fb6a2a2b99e0aab8b3a
Guilherme Oliveira <gui.dev.oliveira@gmail.com>
2025-05-01 11:49:31 -0300

    chore: Ajustado o componente main App, para usar os novos componentes criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
 import './styles/theme.css'
 import './styles/global.css'
+import { Container } from './components/Container'
+import { Heading } from './components/Heading'
+import { Logo } from './components/Logo'
 
 export default function App() {
 	return (
 		<>
-			<div className='container'>
-				<div className='content'>
-					<section>
-						logo
-					</section>
-				</div>
-			</div>
-			<div className='container'>
-				<div className='content'>
-					<section>
-						Menu
-					</section>
-				</div>
-			</div>
-			<div className='container'>
-				<div className='content'>
-					<section>
-						Form
-					</section>
-				</div>
-			</div>
-			<div className='container'>
-				<div className='content'>
-					<section>
-						Footer
-					</section>
-				</div>
-			</div>
+			<Container>
+				<Logo />
+			</Container>
+			<Container>
+				<Heading>Menu</Heading>
+			</Container>
+			<Container>
+				<Heading>Form</Heading>
+			</Container>
+			<Container>
+				<Heading>Footer</Heading>
+			</Container>
 		</>
 	)
 }
