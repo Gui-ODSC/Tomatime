4f9befe911755c11edde174195a9ad3334b241d8
Guilherme Oliveira <gui.dev.oliveira@gmail.com>
2025-05-04 18:04:36 -0300

    chore: Ajuste nas props do componente Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,11 +7,11 @@ type InputProps = {
     labelText?: string;
 } & React.ComponentProps<'input'>;
 
-export function Input({ type, id, labelText, ...rest }: InputProps) {
+export function Input({ type, id, labelText, ...props }: InputProps) {
     return (
         <>
             {labelText && <label htmlFor={id}>{labelText}</label>}
-            <input className={styles.input} id={id} type={type} {...rest} />
+            <input className={styles.input} id={id} type={type} {...props} />
         </>
     )
 }
