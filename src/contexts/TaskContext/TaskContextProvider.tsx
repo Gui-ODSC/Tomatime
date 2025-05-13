f5693b87b2de676d70451ecee2fdff1fe8999b63
Gui Oliveira <guilhermecabrera51@gmail.com>
2025-05-13 20:13:03 -0300

    chore: criado o componente de context provider

---
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -0,0 +1,17 @@
+import { initialTaskState } from "./initialTaskState";
+import { useState } from "react";
+import { TaskContext } from "./TaskContext";
+
+type TaskContextProviderProps = {
+    children: React.ReactNode;
+}
+
+export function TaskContextProvider({ children }: TaskContextProviderProps) {
+    const [state, setState] = useState(initialTaskState);
+
+    return (
+        <TaskContext.Provider value={{ state, setState }}>
+            {children}
+        </TaskContext.Provider>
+    );
+}
